e5699588b2c90a46d55d4df8bafebf030ffaa515
Henrique <henriquetmm@gmail.com>
2018-08-01 09:33:31 -0400

    added messages.cs

---
+++ KokoTalk/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupAss
+{
+    public class Message
+    {
+        public string sender { set; get; }
+        public string receiver { set; get; }
+        public string time { set; get; }
+        public string message { set; get; }
+
+    }
+}
