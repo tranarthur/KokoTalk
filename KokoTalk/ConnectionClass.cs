6fe399fdc60988c8e76022c81de9fe786abbbd2c
Henrique <henriquetmm@gmail.com>
2018-08-09 21:22:32 -0400

    Test Henrique

---
+++ KokoTalk/ConnectionClass.cs
@@ -0,0 +1,31 @@
+﻿/*Author: Dennis Suarez
+ *Student ID: 991 461 216
+ *Date: 2018-07-09
+ */
+using System;
+using System.Data.SqlClient;
+
+namespace KokoTalk
+{
+    public class ConnectionSQL
+    {
+        private static SqlConnection conn;
+
+        public static SqlConnection connectDB(string userName)
+        {
+            string connectionString = "Data Source=" + userName + "\\SQLEXPRESS;Initial Catalog=KokoTalkDB;"
+                + " Integrated Security = SSPI; Persist Security Info = False";
+
+            conn = new SqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot connect to the Database! " + e.Message);
+            }
+            return conn;
+        }
+    }
+}
